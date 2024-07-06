984ed749e516622933fe711f0c13186d5b23756f
vsakkas <vasileios.sakkas96@gmail.com>
2024-07-06 15:29:52 +0200

    Add basic implementation (#2)
    - Add basic implementation of `DjangoBaseSettings` class
    - Update README usage section

---
+++ django_base_settings/django_base_settings.py
@@ -0,0 +1,44 @@
+import sys
+
+from pydantic_settings import BaseSettings
+
+
+class DjangoBaseSettings(BaseSettings):
+    """
+    Base class for Django application settings that allows values to be
+    overridden by environment variables.
+
+    ### Usage
+
+    In your Django settings file, define a subclass of `DjangoBaseSettings`:
+
+    ```python
+    class MySiteSettings(DjangoBaseSettings):
+        allowed_hosts: list[str] = ["www.example.com"]
+        debug: bool = False
+        default_from_email: str = "webmaster@example.com"
+
+    my_site_settings = MySiteSettings()
+    ```
+
+    This is equivalent to:
+
+    ```python
+    ALLOWED_HOSTS = ["www.example.com"]
+    DEBUG = False
+    DEFAULT_FROM_EMAIL = "webmaster@example.com"
+    ```
+
+    Field names in `DjangoBaseSettings` are case-insensitive and can be
+    overridden through environment variables.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Get the module where this instance was created
+        module = sys.modules[self.__class__.__module__]
+
+        # Inject validated fields as module-level variables
+        for field_name, field_value in self.model_dump().items():
+            setattr(module, field_name.upper(), field_value)
