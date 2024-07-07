1a5dcdcd1570b72e6cbcb5ef84221ba193f1276f
vsakkas <vasileios.sakkas96@gmail.com>
2024-07-07 14:08:05 +0200

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+from django_base_settings import BaseSettings, DjangoBaseSettings
+
+
+# Simple settings
+class SimpleSettings(DjangoBaseSettings):
+    allowed_hosts: list[str] = ["www.example.com"]
+    debug: bool = False
+    default_from_email: str = "webmaster@example.com"
+
+
+# Nested settings
+class CacheSettings(BaseSettings):
+    backend: str = "django.core.cache.backends.redis.RedisCache"
+    location: str = "redis://127.0.0.1:6379/1"
+
+
+class NestedSettings(DjangoBaseSettings):
+    caches: dict[str, CacheSettings] = {"default": CacheSettings()}
+
+
+@pytest.fixture
+def simple_settings():
+    return SimpleSettings()
+
+
+@pytest.fixture
+def nested_settings():
+    return NestedSettings()
